115eb5ae0687d171a30e09f5764d1c9ac3a06415
Kenneth Kan <ken@pixbi.com>
2014-10-06 21:11:23 -0400

    Use default shake directory

--- app/util/build.sh
+++ app/util/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p _shake
-ghc --make app/src/Main.hs -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build "$@"
+mkdir -p .shake
+ghc --make app/src/Main.hs -rtsopts -with-rtsopts=-I0 -outputdir=.shake -o .shake/build && .shake/build "$@"
