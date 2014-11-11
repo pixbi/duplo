cb2d069dad02d162094d8ba3ae226069a91af643
Kenneth Kan <ken@pixbi.com>
2014-11-11 13:42:35 -0500

    Bump version

--- src/CompileJS.hs
+++
@@ -1,9 +0,0 @@
-import Language.JavaScript.Parser (readJs, renderToString)
-import Development.Duplo.JavaScript.Order (order)
-
-main = do
-  content <- getContents
-  let precompiled = readJs content
-  let ordered = order precompiled
-  let compiled = ordered
-  putStr $ renderToString compiled
