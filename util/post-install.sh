4c2ec947ea4c48372dcf23f8c49ecdea88340a6d
Yazhong Liu <yorkiefixer@gmail.com>
2014-12-18 12:37:15 +0800

    test: using chai.js
    should.js has a bug on integrating with browserStack

--- util/post-install.sh
+++ util/post-install.sh
@@ -6,5 +6,5 @@ tar=etc/test/vendor/
 # moving files
 cp $src/mocha/mocha.js $tar
 cp $src/mocha/mocha.css $tar
-cp $src/should/should.js $tar
+cp $src/chai/chai.js $tar
 cp $src/sinon/pkg/sinon.js $tar
