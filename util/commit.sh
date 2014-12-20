0f05b97c66972f741232823ce4a01f75626801f3
Kenneth Kan <ken@pixbi.com>
2014-12-19 19:51:34 -0500

    Drop production branch support as we always want to just version

--- util/commit.sh
+++ util/commit.sh
@@ -2,23 +2,14 @@
 
 version=$1
 
-# Always update the local branch first; otherwise we may have merging issue
-# with production later on.
+# Always update the local branch first.
 git pull origin master
 # Commit to master.
 git add component.json
 git commit -m "Bump version to v"$version
-# Merge into production branch.
-git checkout production
-# Update local production branch first.
-git pull origin production
-# Always force the new changes from master because the master branch should
-# always be ahead.
-git merge master -X theirs
 # Apply tag for release.
 git tag $version
 # Sync both branches and the new tag with remote.
-git push origin production:production
 git push origin master:master
 git push origin --tags
 # Continue working on master.
--- util/init-git.sh
+++ util/init-git.sh
@@ -8,5 +8,3 @@ git add . --all
 git commit -m 'Initial'
 # Make sure we have a primary remote repository
 git remote add origin "git@github.com:"$name".git"
-# Create branch for production
-git checkout -b production
